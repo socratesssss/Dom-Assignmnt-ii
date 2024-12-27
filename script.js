4b740283c9a79561e3d2d57392fac282e09b2fa3
socratesssss <studentidsijan@gmail.com>
2024-12-27 11:30:04 +0600

    commit-6-    // Creating a edit btn button in new li

--- script.js
+++ script.js
@@ -33,15 +33,24 @@ addbtn.addEventListener("click", (e) => {
     const btnlist = document.createElement("div");
     btnlist.className = "flex";
 
-    // Creating a checkbox type button
+    // Creating a checkbox type button in new li
     const liradio = document.createElement("span");
     liradio.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mx-2 stroke-2 active:stroke-green-500">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
             </svg>`;
 
+             // Creating a edit btn button in new li
+
+            const edtbtn = document.createElement("span");
+            edtbtn.innerHTML = `
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 mx-2 stroke-2 stroke-black">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>`;
+   li.appendChild(span)
+   btnlist.appendChild(edtbtn)
    btnlist.appendChild(liradio)
-    ullist.appendChild(btnlist);
-    ullist.appendChild(span)
+    li.appendChild(btnlist);
+    
     ullist.appendChild(li)
 })
